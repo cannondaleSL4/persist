8a08e798a35ef9cfdd670e332663f220935789f5
dmitriy <workday2006@gmail.com>
2018-05-21 00:16:10 +0300

    init

--- src/main/java/com/repository/QuotesRepository.java
+++ src/main/java/com/repository/QuotesRepository.java
@@ -1,6 +1,6 @@
 package com.repository;
 
-import com.dim.fxapp.entity.Quotes;
+import com.dim.fxapp.entity.impl.quotes.AUDUSD;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,5 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * Created by dima on 21.05.18.
  */
 @RepositoryRestResource(collectionResourceRel = "quotes", path = "quotes")
-public interface QuotesRepository extends Repository<Quotes>,PagingAndSortingRepository<Quotes, Integer> {
+public interface QuotesRepository extends Repository<AUDUSD>,PagingAndSortingRepository<AUDUSD, Integer> {
 }
