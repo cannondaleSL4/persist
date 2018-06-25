20b6e4c2cf61f74dacc9210c60a6a4b8b19e6e80
dmitriy <workday2006@gmail.com>
2018-06-25 22:40:03 +0300

    add request repository with date

--- src/main/java/com/PersistApplications.java
+++ src/main/java/com/PersistApplications.java
@@ -1,12 +1,8 @@
 package com;
 
-import com.repository.MongoQuotesRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
  * Created by dima on 07.05.18.
